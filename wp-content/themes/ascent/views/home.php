a375c71c9984819e9eda780173b075de40915d1f
Bing Nguyen <bingnguyen@gmail.com>
2015-09-23 01:28:57 +0700

    [wp-01] search function

--- wp-content/themes/ascent/views/home.php
+++ wp-content/themes/ascent/views/home.php
@@ -2,7 +2,7 @@
 //wp-content\themes\ascent\content.php
 ?>
 <div class="col-sm-12 col-md-9">
-	<article id="post-{{post.ID}}" ng-repeat="post in posts" class="post-1 post type-post status-publish format-standard hentry category-uncategorized">
+	<article id="post-{{post.ID}}" ng-repeat="post in posts | filter:searchquery" class="post-1 post type-post status-publish format-standard hentry category-uncategorized">
 		<div class="row">
 			<div class="post-meta-info col-sm-12 col-md-2">
 			<div class="entry-meta" ng-if="post.type == 'post'">
@@ -54,4 +54,38 @@
 </div>
 
 <div class="col-sm-12 col-md-3">
+
+	<div class="sidebar">
+
+	    <?php // add the class "panel" below here to wrap the sidebar in Bootstrap style ;) ?>
+	    <div class="sidebar-padder">
+
+		    <aside id="search" class="widget widget_search">
+				<form role="search" method="get" class="search-form" action="/">
+					<label>
+						<input ng-model="searchquery" type="search" class="search-field form-control" placeholder="Search ..." value="" name="s" title="Search for:">
+					</label>
+					<input type="submit" class="search-submit btn btn-default" value="Search">
+				</form>
+		    </aside>
+
+		    <aside id="archives" class="widget widget_archive">
+			<h3 class="widget-title">Archives</h3>
+			<ul>
+			    <?php //wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+			</ul>
+		    </aside>
+
+		    <aside id="meta" class="widget widget_meta"> 
+			<h3 class="widget-title">Meta</h3>
+			<ul>
+			    <?php //wp_register(); ?>
+			    <li><?php //wp_loginout(); ?></li>
+			    <?php //wp_meta(); ?>
+			</ul>
+		    </aside>
+
+	    </div><!-- close .sidebar-padder -->
+	</div><!-- close .sidebar -->
+
 </div>
